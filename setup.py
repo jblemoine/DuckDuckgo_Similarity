066da75b99aafadd69cac6d7dcb853730b6a2234
jb <j.lemoine@tbs-education.org>
2018-03-30 15:37:04 +0200

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DuckDuckGo_Similarity',
+    version='0.1',
+    author='Jean-Baptiste Lemoine',
+    author_email='j.lemoine@tbs-education.org',
+    include_package_data=True,
+    description='Similarity engine based on DuckDuckGo.',
+    install_requires=['spacy']
+
+)
