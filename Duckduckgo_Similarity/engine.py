5b56706a4eb4f7e98ba4be28a93059244c6cd40d
jb <j.lemoine@tbs-education.org>
2018-03-30 22:54:31 +0200

    Add new features and notebook performance analysis.

--- Duckduckgo_Similarity/engine.py
+++ Duckduckgo_Similarity/engine.py
@@ -1,23 +1,41 @@
 import spacy
 from json.decoder import JSONDecodeError
+from googletrans import Translator
 from .duckduckgo import duckduckgo
 
 
 def extract_feature(strg: str):
+
+    # pick the best language between french and english
     r = duckduckgo.query(strg)
 
-    features = {
-        'abstract_text': r.abstract.text,
-        'abstract_url': r.abstract.url,
-        'image_url': r.image.url,
-        'best_result': ''
-    }
+    if r.abstract.text:
 
-    try:
-        features['best_result'] = duckduckgo.get_zci(strg)
-    # handling of unknown error
-    except JSONDecodeError:
-        pass
+        features = {
+            'abstract_text': r.abstract.text,
+            'abstract_url': r.abstract.url.split('/')[-1],
+            'image_url': r.image.url.split('/')[-1],
+            'best_result': '',
+        }
+
+    # if nothing found in english get the french version and translate in english
+    else:
+        r = duckduckgo.query(strg, kad='fr_Fr')
+        tr = Translator()
+
+        features = {
+            'abstract_text': tr.translate(r.abstract.text, dest='en').text,
+            'abstract_url': r.abstract.url.split('/')[-1],
+            'image_url': r.image.url.split('/')[-1],
+            'best_result': '',
+
+        }
+
+    if not features['abstract_text']:
+        try:
+            features['best_result'] = duckduckgo.get_zci(strg)
+        except JSONDecodeError:
+            pass
 
     return features
 
@@ -29,16 +47,24 @@ class DdgToken:
 
     nlp = spacy.load('en')
     _instances = []
+    _instances_string = []
 
     def __init__(self, string):
 
         self.string = string
         self.features = extract_feature(self.string)
-        self._instances.append(self)
+
+        if self.string not in DdgToken._instances_string:
+            DdgToken._instances.append(self)
+            DdgToken._instances_string.append(self.string)
 
     @classmethod
     def get_instances(cls):
-        return cls._instances
+
+        instances = cls._instances
+        cls._instances = []
+        cls._instances_string = []
+        return instances
 
     @property
     def processed_features(self):
@@ -66,11 +92,18 @@ class DdgToken:
         if metric not in allowed_metrics:
             raise ValueError('metric must be either {}.'.format(' ,'.join(allowed_metrics)))
 
-        similarities = {
-            name: self.processed_features[name].similarity(token.processed_features[name]) for name in
-            self.processed_features
-        }
-        values = similarities.values()
+        values = [self.processed_features[name].similarity(token.processed_features[name]) for name in
+                  self.processed_features if (self.processed_features[name] and token.processed_features[name])
+                  ]
+
+        # I add those metrics, since the artist name of pseudonym tend to be found among abstract_text or abstract_url.
+        values.extend(
+            [self.nlp(self.string).similarity(token.processed_features[name])
+             for name in ['abstract_text', 'abstract_url']])
+
+        values.extend(
+            [self.nlp(token.string).similarity(self.processed_features[name])
+             for name in ['abstract_text', 'abstract_url']])
 
         if metric == 'mean':
             return sum(values) / len(values)
